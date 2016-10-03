f5fa99a566eca131e7d26dcf35cbb93257e3c6a8
BuzznBias <nnamdiwill@gmail.com>
2016-10-03 22:40:37 +0000

    adding travis and mlab

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                         'mongodb://nnamdiwill:s4lw2Etbs@ds049466.mlab.com:49466/mongo-shopping-list' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
